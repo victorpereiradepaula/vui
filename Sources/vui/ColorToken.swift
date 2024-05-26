c9ded0fc34f61d65801a5cce81cf5a43e3f345f9
Victor Pereira de Paula <victorpereiradepaula@gmail.com>
2024-05-25 21:11:04 -0400

    Adição de tokens de cor

---
+++ Sources/vui/ColorToken.swift
@@ -0,0 +1,61 @@
+//
+//  ColorToken.swift
+//
+//
+//  Created by Victor Pereira de Paula on 25/05/24.
+//
+
+public protocol ColorToken {
+    var primary: Color { get }
+    var onPrimary: Color { get }
+    var Container: Color { get }
+    var onContainer: Color { get }
+    
+    var secondary: Color { get }
+    var onSecondary: Color { get }
+    var secondaryContainer: Color { get }
+    var onSecondaryContainer: Color { get }
+    
+    var tertiary: Color { get }
+    var onTertiary: Color { get }
+    var tertiaryContainer: Color { get }
+    var onTertiaryContainer: Color { get }
+    
+    var error: Color { get }
+    var onError: Color { get }
+    var errorContainer: Color { get }
+    var onErrorContainer: Color { get }
+    
+    var warning: Color { get }
+    var onWarning: Color { get }
+    var warningContainer: Color { get }
+    var onWarningContainer: Color { get }
+    
+    var success: Color { get }
+    var onSuccess: Color { get }
+    var successContainer: Color { get }
+    var onSuccessContainer: Color { get }
+    
+    var surfaceDim: Color { get }
+    var surface: Color { get }
+    var surfaceBright: Color { get }
+    
+    var surfaceContainerLowest: Color { get }
+    var surfaceContainerLow: Color { get }
+    var surfaceContainer: Color { get }
+    var surfaceContainerHigh: Color { get }
+    var surfaceContainerHighest: Color { get }
+    var onSurface: Color { get }
+    var onSurfaceVariant: Color { get }
+    
+    var inverseSurface: Color { get }
+    var inverseOnSurface: Color { get }
+    var inversePrimary: Color { get }
+    
+    var outline: Color { get }
+    var outlineVariant: Color { get }
+    
+    var scrim: Color { get }
+    
+    var shadow: Color { get }
+}
