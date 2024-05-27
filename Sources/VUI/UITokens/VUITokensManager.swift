728603fad64ab93bf9f8981302b6f7a3ce473721
Victor Pereira de Paula <victorpereiradepaula@gmail.com>
2024-05-27 00:46:21 -0400

    Sistema de gerenciuamento de userInterfaceStyle

--- Sources/VUI/UITokens/VUITokensManager.swift
+++ Sources/VUI/UITokens/VUITokensManager.swift
@@ -10,17 +10,25 @@ import Foundation
 open class VUITokensManager {
     private var lightColorTokens: ColorTokens = DefaultColorTokens()
     private var darkColorTokens: ColorTokens = DefaultColorTokens()
+    private var userInterfaceStyle: UIUserInterfaceStyle?
     
     public static var shared = VUITokensManager()
     
     private init() {}
     
     var colorTokens: ColorTokens {
-        UITraitCollection.current.userInterfaceStyle == .dark ? darkColorTokens : lightColorTokens
+        (userInterfaceStyle ?? UITraitCollection.current.userInterfaceStyle) == .dark ? darkColorTokens : lightColorTokens
     }
     
     public func configure(_ lightColorTokens: ColorTokens, _ darkColorTokens: ColorTokens? = nil) {
         self.lightColorTokens = lightColorTokens
         self.darkColorTokens = darkColorTokens ?? lightColorTokens
+    }
+    
+    public func setUserInterfaceStyle(_ userInterfaceStyle: UIUserInterfaceStyle) {
+        self.userInterfaceStyle = userInterfaceStyle
+        UIApplication.shared.windows.forEach { window in
+            window.overrideUserInterfaceStyle = userInterfaceStyle
+        }
     }
 }
